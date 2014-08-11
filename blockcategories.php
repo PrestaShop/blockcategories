0ba6213e29d093ed6af3668fc01db383ccb45d1d
Jerome Nadaud <jerome.nadaud@gmail.com>
2014-08-11 09:32:38 +0200

    //Version update

--- blockcategories.php
+++ blockcategories.php
@@ -33,7 +33,7 @@ class BlockCategories extends Module
 	{
 		$this->name = 'blockcategories';
 		$this->tab = 'front_office_features';
-		$this->version = '2.8.1';
+		$this->version = '2.8.2';
 		$this->author = 'PrestaShop';
 
 		$this->bootstrap = true;
