e82eeea329086c7443dae8c8c1eca900abf18398
Thomas N <thomas.nabord@prestashop.com>
2015-08-20 14:48:40 +0200

    // Version updated to v2.9.1

--- blockcategories.php
+++ blockcategories.php
@@ -33,7 +33,7 @@ class BlockCategories extends Module
 	{
 		$this->name = 'blockcategories';
 		$this->tab = 'front_office_features';
-		$this->version = '2.9.0';
+		$this->version = '2.9.1';
 		$this->author = 'PrestaShop';
 
 		$this->bootstrap = true;
